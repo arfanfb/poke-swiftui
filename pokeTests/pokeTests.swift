525656a3850fe57db8d3933b5f61cc12123df8f0
arfan.tkp103@digital.telkom.co.id <arfan.febriyanto@digital.telkom.co.id>
2025-02-25 15:37:12 +0700

    feat: init project pokemon swiftui

---
+++ pokeTests/pokeTests.swift
@@ -0,0 +1,17 @@
+//
+//  pokeTests.swift
+//  pokeTests
+//
+//  Created by Arfan on 25/02/25.
+//
+
+import Testing
+@testable import poke
+
+struct pokeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
